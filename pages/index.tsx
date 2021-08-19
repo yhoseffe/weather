b043d6493942d9bf184ce740cc3eac3df609b338
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-19 17:30:03 +0800

    Update box with attributes

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,14 +44,14 @@ function weatherBox() {
     <div style={{ 
         width: 200
      }}>
-      {/* <Autocomplete
+      <Autocomplete
         id="free-solo-demo"
         freeSolo
         options={top100Films.map((option) => option.title)}
         renderInput={(params) => (
           <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
         )}
-      /> */}
+      />
       <Autocomplete
         freeSolo
         id="free-solo-2-demo"
