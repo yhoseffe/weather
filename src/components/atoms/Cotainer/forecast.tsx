51edbedb0e387c22ae3c7f0ce649976867174ffc
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-20 19:51:37 +0800

    add forecast container

--- src/components/atoms/Cotainer/forecast.tsx
+++ src/components/atoms/Cotainer/forecast.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
+import React from 'react';
+// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     root: {
