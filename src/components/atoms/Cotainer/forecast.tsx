8839d7876946dca605cb00d5debd5dc1e978b9ff
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-20 23:42:20 +0800

    add forecast container

--- src/components/atoms/Cotainer/forecast.tsx
+++ src/components/atoms/Cotainer/forecast.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(10),
       textAlign: 'center',
       color: theme.palette.text.secondary,
+      variant: "h3",
           
     },
   }))
