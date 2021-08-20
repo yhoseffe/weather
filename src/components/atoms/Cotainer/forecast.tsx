7c4094bf067bb79e86f6825e92079c9edc6d852b
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-20 20:21:06 +0800

    add forecast container

--- src/components/atoms/Cotainer/forecast.tsx
+++ src/components/atoms/Cotainer/forecast.tsx
@@ -1,37 +1,67 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       flexGrow: 1,
+//       maxWith: 1200,
+//       margin: 'auto',
+//     },
+//     paper: {
+//       padding: theme.spacing(2),
+//       textAlign: 'center',
+//       color: theme.palette.text.secondary,
+//     },
+//   }))
+
+// export default function CenteredGrid() {
+//   const classes = useStyles()
+
+//   return (
+//     <div className={classes.root}>
+//       <Grid container spacing={3}>
+//         <Grid item xs>
+//           <Paper className={classes.paper}>Weather weather lang</Paper>
+//         </Grid>
+//         <Grid item xs>
+//           <Paper className={classes.paper}>Weather weather lang</Paper>
+//         </Grid>
+//         <Grid item xs>
+//           <Paper className={classes.paper}>Weather weather lang</Paper>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     root: {
-      flexGrow: 1,
-      maxWith: 1200,
-      margin: 'auto',
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
     },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }))
+  }),
+);
 
 export default function CenteredGrid() {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>Weather weather lang</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>Weather weather lang</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>Weather weather lang</Paper>
-        </Grid>
-      </Grid>
+      <Paper elevation={0} />
+      <Paper />
+      <Paper elevation={3} />
     </div>
   );
 }
