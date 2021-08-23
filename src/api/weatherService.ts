2429829b735b23ae82ae72dd6c5cf66eea67d9e8
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-23 21:08:06 +0800

    fdsafd

--- src/api/weatherService.ts
+++ src/api/weatherService.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getSuggestedPlaces = async (query: string) => {
-return await axios.get(`http://api.weatherapi.com/v1/search.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`)
+return await axios.get(`https://api.weatherapi.com/v1/search.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`)
 
 
 }
