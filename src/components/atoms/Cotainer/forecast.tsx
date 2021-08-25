73defcd476ce040b3bc136733c1f99b76f03e74a
Joseph Bautista <josepharielbautista@gmail.com>
2021-08-25 21:02:20 +0800

    Add useState

--- src/components/atoms/Cotainer/forecast.tsx
+++ src/components/atoms/Cotainer/forecast.tsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      maxWidth: 1200,
-      margin: 'auto',
-    },
-    paper: {
-      height: 200,
-      padding: theme.spacing(10),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,         
-    }  
-  }
- )
-)
+  root: {
+    flexGrow: 1,
+    maxWidth: '100%',
+    margin: 'auto',
+  },
+  paper: {
+    height: 200,
+    padding: theme.spacing(10),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
 
 export default function CenteredGrid() {
   const classes = useStyles()
 
   return (
-    <div className={classes.root} >
-               <Grid container spacing={3}>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
         <Grid item xs>
-          <Paper className={classes.paper} elevation={2}>Yesterday</Paper>
+          <Paper className={classes.paper} elevation={2}>
+            Yesterday
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper} elevation={2}>Today</Paper>
+          <Paper className={classes.paper} elevation={2}>
+            Today
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper} elevation={2}>Tomorrow</Paper>
+          <Paper className={classes.paper} elevation={2}>
+            Tomorrow
+          </Paper>
         </Grid>
       </Grid>
     </div>
-  );
+  )
 }
